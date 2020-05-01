16fc3fd84b0fae8e89c3c79ab092d9df667b2f77
James Lenahan <james.k.lenahan@gmail.com>
2020-05-01 18:26:40 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,3 +1,4 @@
+  
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
@@ -6,14 +7,14 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map( movie => <MovieCard title={movie.title} IMDBRating = {movie.IMDBRating} genres = {movie.genres} poster={movie.poster} />)
   }
 
-  render(){
+  render() {
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
       </div>
-      )
-    
+    )
   }
 }
